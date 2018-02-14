574161325d283c758fb2549fcb026381304cc414
SaiyaYe <799139468@qq.com>
2018-02-14 11:02:07 +0800

    时间选择器控件添加

---
+++ EPS.UI.Portal/Models/DateRangeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EPS.UI.Portal.Models
+{
+    public class DateRangeModel
+    {
+        public string StartDate { get; set; }
+
+        public string EndDate { get; set; }
+    }
+}
